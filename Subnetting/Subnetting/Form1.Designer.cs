168f876ea0b97e5c625a649f5d6355b5ad2dfa50
Andrea Marano <andreamarano2004@gmail.com>
2022-03-02 09:48:58 +0100

    Better UI

--- Subnetting/Subnetting/Form1.Designer.cs
+++ Subnetting/Subnetting/Form1.Designer.cs
@@ -30,14 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblIP = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
             this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
-            this.kryptonLabel3 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
-            this.txtIP = new System.Windows.Forms.TextBox();
-            this.txtMask = new System.Windows.Forms.TextBox();
-            this.txtHost = new System.Windows.Forms.TextBox();
-            this.btnInserisci = new MaterialSkin.Controls.MaterialButton();
-            this.btnCalcola = new MaterialSkin.Controls.MaterialButton();
+            this.btnInserisci = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.btnCalcola = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.txtIP = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.txtMask = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.txtHost = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
             this.dataGridView1 = new ComponentFactory.Krypton.Toolkit.KryptonDataGridView();
             this.classe = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ipRete = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -53,100 +53,75 @@
             // lblIP
             // 
             this.lblIP.AutoSize = true;
-            this.lblIP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.lblIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.lblIP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.lblIP.Location = new System.Drawing.Point(12, 15);
             this.lblIP.Name = "lblIP";
-            this.lblIP.Size = new System.Drawing.Size(0, 17);
+            this.lblIP.Size = new System.Drawing.Size(0, 13);
             this.lblIP.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(313, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "/";
             // 
             // kryptonLabel1
             // 
-            this.kryptonLabel1.Location = new System.Drawing.Point(6, 113);
+            this.kryptonLabel1.Location = new System.Drawing.Point(23, 15);
             this.kryptonLabel1.Name = "kryptonLabel1";
-            this.kryptonLabel1.Size = new System.Drawing.Size(70, 20);
-            this.kryptonLabel1.TabIndex = 1;
-            this.kryptonLabel1.Values.Text = "Num. host:";
+            this.kryptonLabel1.Size = new System.Drawing.Size(88, 20);
+            this.kryptonLabel1.TabIndex = 16;
+            this.kryptonLabel1.Values.Text = "IP di partenza:";
             // 
             // kryptonLabel2
             // 
-            this.kryptonLabel2.Location = new System.Drawing.Point(6, 78);
+            this.kryptonLabel2.Location = new System.Drawing.Point(41, 49);
             this.kryptonLabel2.Name = "kryptonLabel2";
-            this.kryptonLabel2.Size = new System.Drawing.Size(88, 20);
-            this.kryptonLabel2.TabIndex = 2;
-            this.kryptonLabel2.Values.Text = "Ip di partenza:";
-            // 
-            // kryptonLabel3
-            // 
-            this.kryptonLabel3.Location = new System.Drawing.Point(266, 78);
-            this.kryptonLabel3.Name = "kryptonLabel3";
-            this.kryptonLabel3.Size = new System.Drawing.Size(15, 20);
-            this.kryptonLabel3.TabIndex = 3;
-            this.kryptonLabel3.Values.Text = "/";
-            // 
-            // txtIP
-            // 
-            this.txtIP.Location = new System.Drawing.Point(100, 78);
-            this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(160, 20);
-            this.txtIP.TabIndex = 4;
-            // 
-            // txtMask
-            // 
-            this.txtMask.Location = new System.Drawing.Point(287, 78);
-            this.txtMask.Name = "txtMask";
-            this.txtMask.Size = new System.Drawing.Size(44, 20);
-            this.txtMask.TabIndex = 5;
-            // 
-            // txtHost
-            // 
-            this.txtHost.Location = new System.Drawing.Point(100, 113);
-            this.txtHost.Name = "txtHost";
-            this.txtHost.Size = new System.Drawing.Size(60, 20);
-            this.txtHost.TabIndex = 6;
+            this.kryptonLabel2.Size = new System.Drawing.Size(70, 20);
+            this.kryptonLabel2.TabIndex = 17;
+            this.kryptonLabel2.Values.Text = "Num. host:";
             // 
             // btnInserisci
             // 
-            this.btnInserisci.AutoSize = false;
-            this.btnInserisci.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnInserisci.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            this.btnInserisci.Depth = 0;
-            this.btnInserisci.HighEmphasis = true;
-            this.btnInserisci.Icon = null;
-            this.btnInserisci.Location = new System.Drawing.Point(173, 113);
-            this.btnInserisci.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.btnInserisci.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnInserisci.Location = new System.Drawing.Point(250, 46);
             this.btnInserisci.Name = "btnInserisci";
-            this.btnInserisci.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btnInserisci.Size = new System.Drawing.Size(158, 20);
-            this.btnInserisci.TabIndex = 7;
-            this.btnInserisci.Text = "nuova sottorete";
-            this.btnInserisci.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.btnInserisci.UseAccentColor = false;
-            this.btnInserisci.UseVisualStyleBackColor = true;
+            this.btnInserisci.Size = new System.Drawing.Size(144, 25);
+            this.btnInserisci.TabIndex = 4;
+            this.btnInserisci.Values.Text = "Inserisci nuova sottorete";
             this.btnInserisci.Click += new System.EventHandler(this.btnInserisci_Click);
             // 
             // btnCalcola
             // 
-            this.btnCalcola.AutoSize = false;
-            this.btnCalcola.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnCalcola.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            this.btnCalcola.Depth = 0;
-            this.btnCalcola.HighEmphasis = true;
-            this.btnCalcola.Icon = null;
-            this.btnCalcola.Location = new System.Drawing.Point(6, 447);
-            this.btnCalcola.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.btnCalcola.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnCalcola.Location = new System.Drawing.Point(15, 459);
             this.btnCalcola.Name = "btnCalcola";
-            this.btnCalcola.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btnCalcola.Size = new System.Drawing.Size(1042, 46);
-            this.btnCalcola.TabIndex = 9;
-            this.btnCalcola.Text = "Calcola";
-            this.btnCalcola.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.btnCalcola.UseAccentColor = false;
-            this.btnCalcola.UseVisualStyleBackColor = true;
+            this.btnCalcola.Size = new System.Drawing.Size(1042, 31);
+            this.btnCalcola.TabIndex = 5;
+            this.btnCalcola.Values.Text = "Calcola";
             this.btnCalcola.Click += new System.EventHandler(this.btnCalcola_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(117, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(183, 23);
+            this.txtIP.TabIndex = 1;
+            // 
+            // txtMask
+            // 
+            this.txtMask.Location = new System.Drawing.Point(331, 12);
+            this.txtMask.Name = "txtMask";
+            this.txtMask.Size = new System.Drawing.Size(31, 23);
+            this.txtMask.TabIndex = 2;
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(117, 46);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(127, 23);
+            this.txtHost.TabIndex = 3;
             // 
             // dataGridView1
             // 
@@ -160,10 +135,10 @@
             this.nhost,
             this.rnhost,
             this.spreco});
-            this.dataGridView1.Location = new System.Drawing.Point(6, 152);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 88);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1042, 286);
-            this.dataGridView1.TabIndex = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1042, 353);
+            this.dataGridView1.TabIndex = 23;
             // 
             // classe
             // 
@@ -220,14 +195,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 502);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnCalcola);
-            this.Controls.Add(this.btnInserisci);
             this.Controls.Add(this.txtHost);
             this.Controls.Add(this.txtMask);
             this.Controls.Add(this.txtIP);
-            this.Controls.Add(this.kryptonLabel3);
+            this.Controls.Add(this.btnCalcola);
+            this.Controls.Add(this.btnInserisci);
             this.Controls.Add(this.kryptonLabel2);
             this.Controls.Add(this.kryptonLabel1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.lblIP);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -242,14 +217,14 @@
         #endregion
 
         private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label label3;
         private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
         private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
-        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel3;
-        private System.Windows.Forms.TextBox txtIP;
-        private System.Windows.Forms.TextBox txtMask;
-        private System.Windows.Forms.TextBox txtHost;
-        private MaterialSkin.Controls.MaterialButton btnInserisci;
-        private MaterialSkin.Controls.MaterialButton btnCalcola;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnInserisci;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnCalcola;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtIP;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtMask;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtHost;
         private ComponentFactory.Krypton.Toolkit.KryptonDataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn classe;
         private System.Windows.Forms.DataGridViewTextBoxColumn ipRete;
